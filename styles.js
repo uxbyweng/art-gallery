ccfa2ff344c196b0d794e96d8388121c71cf1993
Karsten Weng <karsten@weng.eu>
2026-02-10 10:56:28 +0100

    refactor: move global CSS to style.js and remove main.css

--- styles.js
+++ styles.js
@@ -17,14 +17,35 @@ export default createGlobalStyle`
   color: #111;
 }
 
+/* Media defaults */
+img,
+picture,
+video,
+canvas,
+svg {
+  display: block;
+  max-width: 100%;
+}
+
+/* Form defaults */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
 main {
   max-width: 1100px;
   margin: 0 auto;
   padding: 24px;
 }
+
 h1, h2 {
     margin-bottom: 20px;
 }
+
+/* muss noch entfernt/in die Card Componente ausgelagert werden */
 img.card__image {
     object-fit: cover;
 }
