30063c37ca8b73e6172e8cc06acb06470b75374c
Karsten Weng <karsten@weng.eu>
2026-02-11 18:34:55 +0100

    make navi horizontal

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,34 +1,53 @@
 import Link from "next/link";
 import styled from "styled-components";
 
+const Nav = styled.nav`
+  background-color: rgb(50, 50, 50);
+  position: sticky;
+  bottom: 0px;
+  z-index: 3;
+`;
+
+const List = styled.ul`
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const ListItem = styled.li`
+  flex: 0 0 33%;
+  background: #ff0000;
+`;
+
+const StyledLink = styled(Link)`
+  display: block;
+  width: 100%;
+  text-align: center;
+  padding: 15px 0;
+  color: #fff;
+`;
+
 export default function Navigation() {
   return (
     <Nav aria-label="Main navigation">
       <List>
         <ListItem>
-          <Link href="/" aria-label="Go to Spotlight page">
+          <StyledLink href="/" aria-label="Go to Spotlight page">
             Spotlight
-          </Link>
+          </StyledLink>
         </ListItem>
         <ListItem>
-          <Link href="/gallery/" aria-label="Go to Art Pieces gallery">
+          <StyledLink href="/gallery/" aria-label="Go to Art Pieces gallery">
             Art Pieces
-          </Link>
+          </StyledLink>
         </ListItem>
         <ListItem>
-          <Link href="/favorites/" aria-label="Go to Favorites page">
+          <StyledLink href="/favorites/" aria-label="Go to Favorites page">
             Favorites
-          </Link>
+          </StyledLink>
         </ListItem>
       </List>
     </Nav>
   );
 }
-
-const Nav = styled.nav``;
-
-const List = styled.ul``;
-
-const ListItem = styled.li``;
-
-const StyledLink = styled(Link)``;
